2c17e9a2fc724ed89662921b6c2251b6bd438755
hariharen96 <hariharen1996@gmail.com>
2025-07-29 10:21:00 +0530

    feat: add templates, static files, food items and item details page Added item_image field in models.py and migrated to db Fetched food item data from the model and rendered it in templates Created base.html with navbar and extended it in other templates using {% extends %} Implemented detail pages based on item ID and added URL routing Installed pillow library for image support

--- food/models.py
+++ food/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_price = models.IntegerField()
     item_desc = models.CharField(max_length=200)
+    item_image = models.ImageField(upload_to='images/',default='images/default.png')
     
     def __str__(self):
         return self.item_name
--- food/views.py
+++ food/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Index Page</h1>')
+    item_list = Item.objects.all()
+    return render(request,'food/index.html',{'items': item_list})
 
 def item(request):
     return HttpResponse('<h1>item Page</h1>')
+
+def detail(request,item_id):
+    item = Item.objects.get(pk=item_id)
+    return render(request,'food/detail.html',{'item':item})
